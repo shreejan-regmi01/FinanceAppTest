a862a419bec9b2384c01acbacec0987088e85c8c
Shreejan Regmi <shreejan.regmi@gmail.com>
2022-07-28 06:09:28 +0545

    configured viewport & baseURL

--- cypress.config.js
+++ cypress.config.js
@@ -5,5 +5,9 @@ module.exports = defineConfig({
     setupNodeEvents(on, config) {
       // implement node event listeners here
     },
+    viewportWidth: 1920,
+    viewportHeight: 1080,
+    video: true,
+    baseUrl: "http://app-qa.carclarity.com.au/",
   },
 });
